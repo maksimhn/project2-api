4f29f885999477716d7f97de4b530b7f3a79fa2f
Maksim <dollydasheep@gmail.com>
2015-07-24 14:21:33 -0400

    Add User.rb new token generation
    Removed private for now until a fix ready

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,16 @@ class User < ActiveRecord::Base
   # ensures email's uniqueness on AR level
   validates :email, uniqueness: true
 
-  private
+  # login action that happens
+  def self.login(email, password)
+    user = find_by email: email
+    # if user is found and password passed to 'authenticate' method returns us user (look 'authenticate' method for details) we just have a user in 'user' variable, not 'nil'. If user hasn't been found we won't go further
+    user = user.authenticate password if user
+    # upon login we create and save new token to the user to kick other machine's authentication out if we logged in from a new machine
+    user.set_token && user.save! if user
+    # returns token if such user has been found
+    user.token if user
+  end
 
   def set_token
     # make it random with 16 (default) URL-safe characters

1278ac305104422cc5cdb3fd98659f0a3328259f
Maksim <dollydasheep@gmail.com>
2015-07-24 13:55:54 -0400

    Add token to User model
    User instance now sets random token on create.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,16 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  # makes sure set_token gets called before creation
+  before_create :set_token
+
+  # ensures email's uniqueness on AR level
   validates :email, uniqueness: true
+
+  private
+
+  def set_token
+    # make it random with 16 (default) URL-safe characters
+    self.token = SecureRandom.hex
+  end
 end

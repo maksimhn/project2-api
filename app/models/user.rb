8df4079e546e613b2cec43eaa1b5edeb2ae607c5
Maksim <dollydasheep@gmail.com>
2015-07-24 14:47:13 -0400

    Add private access for set_token
    Fix class access issue using instance login method to find token

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,13 +10,14 @@ class User < ActiveRecord::Base
   # login action that happens
   def self.login(email, password)
     user = find_by email: email
-    # if user is found and password passed to 'authenticate' method returns us user (look 'authenticate' method for details) we just have a user in 'user' variable, not 'nil'. If user hasn't been found we won't go further
-    user = user.authenticate password if user
-    # upon login we create and save new token to the user to kick other machine's authentication out if we logged in from a new machine
-    user.set_token && user.save! if user
-    # returns token if such user has been found
-    user.token if user
+    user.login password if user
   end
+
+  def login(password)
+    authenticate(password) && set_token && save! && token
+  end
+
+  private
 
   def set_token
     # make it random with 16 (default) URL-safe characters

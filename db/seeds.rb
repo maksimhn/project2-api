ab4183639520e4b07afc938dd8b9b50da3cda0d0
Maksim <dollydasheep@gmail.com>
2015-08-06 17:13:25 -0400

    Fixed a minor issue

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-# \copy words(word, gender, pic) FROM 'nouns.csv' WITH (FORMAT csv, HEADER true)
 require 'csv'
+
+Word.destroy_all
 
 csv_text = File.read('nouns.csv')
 csv = CSV.parse(csv_text, :headers => true)
@@ -15,4 +8,3 @@ csv.each do |row|
   Word.create!(row.to_hash)
 end
 
-end
